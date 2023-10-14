c4bfc0f7567c5c658c5b8ab714392a83ce16c3b6
giaducdang03 <giaducdang@gmail.com>
2023-10-14 19:36:37 +0700

    fix api user GET username

--- NET1705_FService.API/NET1705_FService.API/Controllers/UserController.cs
+++ NET1705_FService.API/NET1705_FService.API/Controllers/UserController.cs
@@ -46,17 +46,17 @@ namespace NET1705_FService.API.Controllers
             }
         }
 
-        [HttpGet("{id}")]
-        [Authorize]
-        public async Task<IActionResult> GetAccount(string id)
-        {
-            try
-            {
-                var account = await accountService.GetAccountAsync(id);
-                return account == null ? NotFound() : Ok(account);
-            }
-            catch { return BadRequest(); }
-        }
+        //[HttpGet("{id}")]
+        //[Authorize]
+        //public async Task<IActionResult> GetAccount(string id)
+        //{
+        //    try
+        //    {
+        //        var account = await accountService.GetAccountAsync(id);
+        //        return account == null ? NotFound() : Ok(account);
+        //    }
+        //    catch { return BadRequest(); }
+        //}
 
         [HttpGet("{name}")]
         [Authorize]
