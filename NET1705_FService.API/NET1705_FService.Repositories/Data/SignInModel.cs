4699e4f9d41cb6b6d5a5c76f7ea6696a86294b06
giaducdang03 <giaducdang@gmail.com>
2023-11-27 16:32:03 +0700

    change server db

--- NET1705_FService.API/NET1705_FService.Repositories/Data/SignInModel.cs
+++ NET1705_FService.API/NET1705_FService.Repositories/Data/SignInModel.cs
@@ -5,9 +5,9 @@ namespace NET1705_FService.Repositories.Data
 {
     public class SignInModel
     {
-        [Required(ErrorMessage = "Username is required!"), EmailAddress(ErrorMessage = "Must be email address format!")]
+        [Required(ErrorMessage = "Email không được để trống!"), EmailAddress(ErrorMessage = "Vui lòng nhập email hợp lệ!")]
         public required string Email { get; set; }
-        [Required(ErrorMessage = "Password is required!")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống!")]
         [DataType(DataType.Password)]
         [PasswordPropertyText]
         public required string Password { get; set; }
