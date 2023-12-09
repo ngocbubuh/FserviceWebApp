033d585c4785e811e703925e56aa5ba36779ddb6
giaducdang03 <giaducdang@gmail.com>
2023-12-09 09:07:37 +0700

    fix time auto scan + fix bugs

--- NET1705_FService.API/NET1705_FService.API/RunSchedule/Setup/ScanningApartmentPackageSetup.cs
+++ NET1705_FService.API/NET1705_FService.API/RunSchedule/Setup/ScanningApartmentPackageSetup.cs
@@ -8,10 +8,19 @@ namespace NET1705_FService.API.RunSchedule.Setup
     {
         public void Configure(QuartzOptions options)
         {
+            //var jobkey = JobKey.Create(nameof(ScanningApartmentPackage));
+            //options.AddJob<ScanningApartmentPackage>(JobBuilder => JobBuilder.WithIdentity(jobkey))
+            //.AddTrigger(trigger =>
+            //    trigger.ForJob(jobkey).WithCronSchedule("0 0 * * * ?"));
+
             var jobkey = JobKey.Create(nameof(ScanningApartmentPackage));
             options.AddJob<ScanningApartmentPackage>(JobBuilder => JobBuilder.WithIdentity(jobkey))
             .AddTrigger(trigger =>
-                trigger.ForJob(jobkey).WithCronSchedule("0 0 * * * ?"));
+                trigger.ForJob(jobkey).WithDailyTimeIntervalSchedule(
+                    s => 
+                     s.WithIntervalInHours(24)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0))));
         }
     }
 }
